e2654680367638a2e5e0638d3f299e88cc2e4857
Donne <donnebaldemeca@gmail.com>
2025-10-28 11:25:54 -0700

    Add dB

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -17,6 +17,14 @@ type application struct {
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn          string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  time.Duration // nanoseconds
 }
 
 func (app *application) mount() http.Handler {
--- internal/env/env.go
+++ internal/env/env.go
@@ -2,6 +2,9 @@ package env
 
 import (
 	"log"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/lpernett/godotenv"
 )
@@ -12,4 +15,41 @@ func LoadEnv() error {
 		log.Fatal("Error loading .env file")
 	}
 	return err
+}
+
+func GetString(key string, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsInt
+}
+
+func GetDuration(key string, fallback time.Duration) time.Duration {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	valAsDuration, err := time.ParseDuration(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsDuration
 }
