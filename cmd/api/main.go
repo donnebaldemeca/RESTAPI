1b9f339996a94d51478237f1e4ba4741e56ab5ff
Donne <donnebaldemeca@gmail.com>
2025-10-28 11:25:54 -0700

    Add dB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"log"
-	"os"
+	"time"
 
+	"github.com/donnebaldemeca/RESTAPI/internal/db"
 	"github.com/donnebaldemeca/RESTAPI/internal/env"
+	"github.com/donnebaldemeca/RESTAPI/internal/storage"
 )
 
 func main() {
 	env.LoadEnv()
 
 	cfg := config{
-		addr: os.Getenv("ADDR"),
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			dsn:          env.GetString("DB_DSN", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetDuration("DB_MAX_IDLE_TIME", 15*time.Minute),
+		},
 	}
 
+	log.Printf("DB_DSN: %v", cfg.db.dsn)
+	log.Printf("DB_MAX_OPEN_CONNS: %d", cfg.db.maxOpenConns)
+	log.Printf("DB_MAX_IDLE_CONNS: %d", cfg.db.maxIdleConns)
+	log.Printf("DB_MAX_IDLE_TIME: %v", cfg.db.maxIdleTime)
+
+	db, err := db.New(
+		cfg.db.dsn,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	storage := storage.NewStorage(db)
+
 	app := &application{
-		config: cfg,
+		config:  cfg,
+		storage: storage,
 	}
 	mux := app.mount()
 
--- go.mod
+++ go.mod
@@ -6,4 +6,4 @@ require github.com/go-chi/chi/v5 v5.2.3
 
 require github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
 
-require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9
